b3051424c1b105280f5ec8a25d66dc22c09f88dc
wildan shobara <wildanshobaradan@gmail.com>
2022-01-10 22:56:16 +0700

    E-commerce App

--- JersiPedia/src/components/besar/HeaderComponent/index.js
+++ JersiPedia/src/components/besar/HeaderComponent/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, StyleSheet, View, TextInput} from 'react-native';
+import {StyleSheet, View, TextInput} from 'react-native';
 import {IconSearch} from '../../../assets/icons';
 import {colors, responsiveHeight, responsiveWidth} from '../../../utils';
 
